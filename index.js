1a25ebde59a9ad3a4497c4f20412eb388b74a005
R. Mompo <rmompo@gmail.com>
2022-09-09 21:19:23 +0200

    Dinamic Port commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get( '*', ( req, res ) => {
     res.sendFile( path.resolve( __dirname, 'public/index.html') );
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor express en puerto ${process.env.PORT}`);
+app.listen(process.env.PORT || process.env.ENV_EXPRESS_PORT, () => {
+    console.log(`Servidor express en puerto ${process.env.PORT || process.env.ENV_EXPRESS_PORT}`);
 });
 
